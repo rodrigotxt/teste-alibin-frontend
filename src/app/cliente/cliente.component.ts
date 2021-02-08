d658fb1a2e694b79687477b07a4d744a196b31ee
Rodrigo Martins <rodrigotxt@gmail.com>
2021-02-08 01:52:16 -0300

    Modulo Cliente

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
   styleUrls: ['./cliente.component.scss']
 })
-export class ClienteComponent implements OnInit {
+export class ClienteComponent{
+	title = 'Cliente'
+  dataClientes = [];
+  id: number;
+  action: string;
+  cliente: any;
+  displayedColumns: string[] = ['id', 'name', 'tel', 'document'];
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.cliente = this.route.params.subscribe(params => {
+      this.id = +params['id'];
+      this.action = params['action'];
+    });
+  }
+  imports:{
+  	
   }
 
 }
