744eed5bd3a982ce2534e4fb33558e5e37cb62f6
Rodrigo Martins <rodrigotxt@gmail.com>
2021-02-07 23:58:22 -0300

    Rotas para Cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteComponent } from './cliente/cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  	{ path: '', redirectTo: 'cliente', pathMatch: 'full' },
+	{ path: 'cliente', component: ClienteComponent },
+	{ path: 'cliente/novo', component: ClienteComponent },
+	{ path: 'cliente/editar/:id', component: ClienteComponent },
+	{ path: 'cliente/view/:id', component: ClienteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
