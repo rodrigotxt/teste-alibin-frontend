59c8df9dac8939d4ca747e9697bde06b5bc06854
Rodrigo Martins <rodrigotxt@gmail.com>
2021-02-09 18:02:22 -0300

    Inseridos comentarios nas funcoes

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -30,13 +30,17 @@ export class ClienteComponent implements OnInit {
   constructor(private router: Router, private route: ActivatedRoute, private _formBuilder: FormBuilder,  private localforage: LocalForageService, private http: HttpClient) { }
 
   ngOnInit(){
+    // verifica se a rota tem os parametros id e action
     this.cliente = this.route.params.subscribe(params => {
       this.id = +params['id'];
       this.action = params['action'];
+      // se for VER ou EDITAR seta o cliente ativo
       if(this.action == 'ver' || this.action == 'editar'){
         this.clienteActive = this.getCliente(this.id, true);
       }
     });
+
+    // inicializacao do form de cadastro
     this.firstFormGroup = this._formBuilder.group({
       id: '',
       name: ['', Validators.required],
@@ -48,10 +52,15 @@ export class ClienteComponent implements OnInit {
       cidade: ['', Validators.required],
       uf: ['', Validators.required]
     });
+    // recarrega lista de clientes
     this.reloadClientes();
+    //busca estados no IBGE
     this.getEstados();
+    // gera lista de produtos
     this.getProdutos();
   }
+
+  // gerar lista de produtos com nome e dados aleatorios para popular um Select
   getProdutos(){
     let cods = [3896,3353,5882,3984,16692,17876,18563,11317,3108,12618,13787,16411];
     let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
@@ -66,14 +75,20 @@ export class ClienteComponent implements OnInit {
     }
     // return this.produtos;
   }
+
+  // vincular produto ao Cliente
   addProduto(produto){
     let cliente = this.clienteActive;
     cliente['itens'].push(produto);
     this.localforage.setItem(cliente.id, cliente);
   }
+
   deleteProduto(index){
    this.clienteActive['itens'].splice(index, 1); 
+   this.localforage.setItem(this.clienteActive.id, this.clienteActive);
   }
+
+  // busca estado na lista do IBGE
   getEstados(){
     return this.http.get<any>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
     .subscribe(data => {
@@ -83,6 +98,8 @@ export class ClienteComponent implements OnInit {
       console.log(err);
     }
   }
+
+  // busca cidade na api do IBGE
   getCidades(e){
     return this.http.get<any>('https://servicodados.ibge.gov.br/api/v1/localidades/estados/' + e.value + '/municipios')
     .subscribe(data => {
@@ -95,6 +112,8 @@ export class ClienteComponent implements OnInit {
       console.log(err);
     }
   }
+
+  // recarrega lista de clientes do banco Local
   reloadClientes(clear = false){
     this.clientes = [];
     this.localforage.keys().subscribe(keys => {
@@ -105,9 +124,13 @@ export class ClienteComponent implements OnInit {
       }
     });
   }
+
+  // redireciona para tela de Edição de ciente
   editarCliente(id){
     this.router.navigate(['/cliente/editar/' + id]);
   }
+
+  // busca cliente pelo id no banco Local
   getCliente(id, active = false){    
     this.localforage.getItem(id).subscribe(res => {
       this.clientes.push(res);
@@ -119,9 +142,12 @@ export class ClienteComponent implements OnInit {
       };
     });
   }
+  // redireciona para rota
   verCliente(cliente){
     this.router.navigate(['/cliente/ver/' + cliente.id]);
   }
+
+  // remove cliente do banco Local e atualiza a lista de clientes
   deleteCliente(id){
     console.log('remove', id);
     this.localforage.removeItem(id).subscribe(res => {
@@ -142,6 +168,9 @@ export class ClienteComponent implements OnInit {
     let form2 = this.secondFormGroup.value;
     dados = {...form1, ...form2};      
     }
+
+    /** Salvar cliente em banco de dados (Local),
+    após redireciona para detalhes do cliente  **/
     this.localforage.setItem(id, dados).subscribe(res => {
       this.getCliente(id);
       this.router.navigate(['/cliente/ver/' + id]);
